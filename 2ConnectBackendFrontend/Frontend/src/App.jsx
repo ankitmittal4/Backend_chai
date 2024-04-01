35be93d991fa310d168ebd5bc71369f7d8112866
Ankit <ankitmittal0814@gmail.com>
2024-04-01 19:05:42 +0530

    update cors

--- 2ConnectBackendFrontend/Frontend/src/App.jsx
+++ 2ConnectBackendFrontend/Frontend/src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
-
+import axios from "axios";
 function App() {
   const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/jokes")
+      .then((res) => {
+        setJokes(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
       <h1>Frontend</h1>
       <h3>Jokes: {jokes.length}</h3>
-      {jokes.map((joke) => (
+      {jokes.map((joke) => {
         <div key={joke.id}>
-          <h3>joke.content</h3>
-          <p>joke.content</p>
-        </div>
-      ))}
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>;
+      })}
     </>
   );
 }
